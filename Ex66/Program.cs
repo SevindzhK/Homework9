500b1996ff9ec04dc2d439a6b428ad9fae0b74f3
SevindzhK <sevinge0709@gmail.com>
2022-09-29 14:31:10 +0300

    Added Ex66

---
+++ Ex66/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+
+Console.Write("Enter first natural number: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter second natural number: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber <= 0 || secondNumber <= 0)
+{
+    Console.WriteLine("Error. Numbers aren't natural");
+}
+Console.WriteLine(PrintSumNumbers(firstNumber, secondNumber));
+
+int PrintSumNumbers(int num1, int num2)
+{
+    if (num1 == num2)
+    {
+        return num1;
+    }
+
+    if (num1 < num2)
+    {
+        return num1 + PrintSumNumbers(num1 + 1, num2);
+    }
+
+    return num2 + PrintSumNumbers(num2 + 1, num1);
+}
+
+
