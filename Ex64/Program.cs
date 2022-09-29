ae753e45df0dae00cac5cd0df14d4867f20ed5d9
SevindzhK <sevinge0709@gmail.com>
2022-09-29 14:30:01 +0300

    Added Ex64

---
+++ Ex64/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+
+Console.WriteLine("Enter natural number: ");
+int number = Convert.ToInt32(Console.ReadLine()); 
+
+if (number <= 0) 
+{
+    Console.WriteLine("Error. The number isn't natural number");
+}
+Console.WriteLine(NumbersRec(number)); 
+
+string NumbersRec(int num)
+{
+    if (num >= 1) return ($"{num}, " + NumbersRec(num - 1));
+    else return String.Empty; 
+}
+
