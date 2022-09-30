b7fdb8f4a4f18a013a5406054edc64d79e64e4b7
SevindzhK <sevinge0709@gmail.com>
2022-09-30 11:11:48 +0300

    Added Ex68

---
+++ Ex68/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n
+
+Console.Write("Enter non-negative first number: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter non-negative second number: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine()); 
+
+if (firstNumber < 0 || secondNumber < 0)
+{
+    Console.WriteLine("Error. Numbers aren't non-negative");
+}
+Console.WriteLine(CalculateAkkermanFunction(firstNumber, secondNumber));
+
+int CalculateAkkermanFunction(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return CalculateAkkermanFunction(m - 1, 1);
+    }
+
+    return CalculateAkkermanFunction(m - 1, CalculateAkkermanFunction(m, n - 1));
+}
+
